2a77c8e896792ae53819432bf392ab0c2affb27a
Sofiafs <sofiasantosf011@gmail.com>
2024-05-09 16:34:16 -0300

    Segundo commit

--- login.py
+++ login.py
@@ -1,4 +1,3 @@
-
 from kivy.app import App 
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
@@ -6,7 +5,49 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.uix.image import Image
-from kivy.utils import get_color_from_hex
+from kivy.uix.screenmanager import ScreenManager,Screen 
+
+class TelaLogin(BoxLayout):
+    def __init__ (self, **kwargs):
+        super(TelaLogin, self).__init__(**kwargs)
+        self.orientation= "vertical"
+        self.padding= [50, 20]
+        self.spacing= 10
+
+        Window.clearcolor= (0, 0, 0.1, 2)
+        self.add_widget(Image(source='/Users/Aluno.sesipaulista/Downloads/images-removebg-preview.png'))
+
+        self.add_widget(Label(text=' L O G I N ', font_size=23))
+
+        self.user_layout = BoxLayout(padding=[0, 10], spacing=10)
+        self.username_label = Label(text="  U S U Á R I O:", font_size=20)
+        self.username_input = TextInput(hint_text='Digite aqui...', multiline=False)
+        self.user_layout.add_widget(self.username_label)
+        self.user_layout.add_widget(self.username_input)
+        
+        self.pass_layout = BoxLayout(padding=[0, 10], spacing=10)
+        self.password_label = Label(text="  S E N H A:", font_size=20)
+        self.password_input = TextInput(hint_text='Digite aqui...', multiline=False)
+        self.pass_layout.add_widget(self.password_label)
+        self.pass_layout.add_widget(self.password_input)
+
+        self.buttons_layout= BoxLayout(padding=[0, 10], spacing=10)
+        self.login_button= Button(text='E N T R A R', color=(0, 0, 0.1, 1), size_hint=(None, None), size=(450, 50),background_color =(100, 100, 100, 100))
+        self.login_button.bind(on_press=self.login)
+        self.signup_button= Button(text='C A D A S T R E - S E', color=(0, 0, 0.1, 1), size_hint=(None,None), size=(450, 50),background_color =(100, 100, 100, 100))
+        self.signup_button.bind(on_press=self.ir_para_segunda_tela)
+        self.buttons_layout.add_widget(self.login_button)
+        self.buttons_layout.add_widget(self.signup_button)
+        self.add_widget(self.buttons_layout)
+
+    def ir_para_segunda_tela(self, *args):
+        self.manager.current = 'Cadastro'
+
+    def login(self,instance):
+        username = self.username_input.text
+        password= self.password_input.text
+        print('Username:', username)
+        print('Password:', password)
 
 
 class TelaCadastro(BoxLayout):
@@ -49,11 +90,14 @@ class TelaCadastro(BoxLayout):
         self.user_layout4.add_widget(self.senha_input)
         self.add_widget(self.user_layout4)
 
-        self.buttons_layout4= BoxLayout(padding=[0, 10], spacing=10)
-        self.signup_button= Button(text='c a d a s t r e - s e',color=(0, 0, 0, 1), size_hint=(None,None), size=(920, 50), background_color =(100, 100, 100, 100))
-        self.signup_button.bind(on_press=self.TelaLogin)
-        self.buttons_layout4.add_widget(self.signup_button)
-        self.add_widget(self.buttons_layout4)
+        self.buttons_layout= BoxLayout(padding=[0, 10], spacing=10)
+        self.signup_button= Button(text='C A D A S T R E - S E', color=(0, 0, 0.1, 1), size_hint=(None,None), size=(900, 50),background_color =(100, 100, 100, 100))
+        self.signup_button.bind(on_press=self.ir_para_primeira_tela)
+        self.buttons_layout.add_widget(self.signup_button)
+        self.add_widget(self.buttons_layout)
+
+    def ir_para_primeira_tela(self, *args):
+        self.parent.parent.current = 'Login'
 
     def cadastro(self,instance):
         username = self.username_input.text
@@ -65,50 +109,22 @@ class TelaCadastro(BoxLayout):
         print('Password:', password)
         print('Número:', numero)
 
-if __name__ == '__main__':
-    TelaCadastro().run() 
+class MyApp(App):
+    def build(self):
+        sm = ScreenManager()
+        tela_login = TelaLogin()
+        tela_cadastro = TelaCadastro()
 
-class TelaLogin(BoxLayout,TelaCadastro):
-    def __init__ (self, **kwargs):
-        super(TelaLogin, self).__init__(**kwargs)
-        self.orientation= "vertical"
-        self.padding= [50, 20]
-        self.spacing= 10
+        screen_login = Screen(name='login')
+        screen_cadastro = Screen(name='cadastro')
 
-        Window.clearcolor= (0, 0, 0.1, 2)
-        self.add_widget(Image(source='/Users/Aluno.sesipaulista/Downloads/images-removebg-preview.png'))
+        screen_login.add_widget(tela_login)
+        screen_cadastro.add_widget(tela_cadastro)
 
-        self.add_widget(Label(text=' L O G I N ', font_size=23))
+        sm.add_widget(screen_login)
+        sm.add_widget(screen_cadastro)
 
-        self.user_layout = BoxLayout(padding=[0, 10], spacing=10)
-        self.username_label = Label(text="  U S U Á R I O:", font_size=20)
-        self.username_input = TextInput(hint_text='Digite aqui...', multiline=False)
-        self.user_layout.add_widget(self.username_label)
-        self.user_layout.add_widget(self.username_input)
-        self.add_widget(self.user_layout)
-        
-        self.pass_layout = BoxLayout(padding=[0, 10], spacing=10)
-        self.password_label = Label(text="  S E N H A:", font_size=20)
-        self.password_input = TextInput(hint_text='Digite aqui...', multiline=False)
-        self.pass_layout.add_widget(self.password_label)
-        self.pass_layout.add_widget(self.password_input)
-        self.add_widget(self.pass_layout)
+        return sm
 
-        self.buttons_layout= BoxLayout(padding=[0, 10], spacing=10)
-        self.login_button= Button(text='l o g i n',color=(0, 0, 0, 1), size_hint=(None, None), size=(450, 50),background_color =(100, 100, 100, 100),)
-        self.login_button.bind(on_press=self.TelaLogin)
-        self.signup_button= Button(text='c a d a s t r e - s e',color=(0, 0, 0, 1), size_hint=(None,None), size=(450, 50), background_color =(100, 100, 100, 100))
-        self.signup_button.bind(on_press=self.TelaCadastro)
-        self.buttons_layout.add_widget(self.login_button)
-        self.buttons_layout.add_widget(self.signup_button)
-        self.add_widget(self.buttons_layout)
-
-    def login(self,instance):
-        username = self.username_input.text
-        password= self.password_input.text
-        print('Username:', username)
-        print('Password:', password)
-
-
-if __name__ == '__main__':
-    TelaLogin().run()
+if __name__ == '_main_':
+    MyApp().run()
